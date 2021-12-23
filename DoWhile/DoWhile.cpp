a2826403629bd10e3cc7fca6ab49e3154265de05
Jesus Conde <educaleft@gmail.com>
2021-12-23 01:59:42 +0000

    Códigos para VT 54 del Curso de CPP Básico, dedicado a los loops Do...While.

--- DoWhile/DoWhile.cpp
+++ DoWhile/DoWhile.cpp
@@ -2,6 +2,82 @@
 
 int main()
 {
+    // seleccion debe estar declarada fuera del do-while para que podamos usarla más tarde
+    int seleccion{};
+
+    do
+    {
+        std::cout << "Por favor, haz una selección: \n";
+        std::cout << "1) Suma\n";
+        std::cout << "2) Resta\n";
+        std::cout << "3) Multiplicación\n";
+        std::cout << "4) División\n";
+        std::cin >> seleccion;
+    } while (seleccion != 1 && seleccion != 2 &&
+        seleccion != 3 && seleccion != 4);
+
+    // hacer algo con la selección aquí
+    // tal como una declaración switch
+
+    std::cout << "Has seleccionado la opción #" << seleccion << "\n";
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+int main()
+{
+    int seleccion{ 0 };
+    bool invalido{ true }; // nueva variable solo para cerrar el loop
+
+    while (invalido)
+    {
+        std::cout << "Por favor, haz una selección: \n";
+        std::cout << "1) Suma\n";
+        std::cout << "2) Resta\n";
+        std::cout << "3) Multiplicación\n";
+        std::cout << "4) División\n";
+
+        std::cin >> seleccion;
+        invalido = (seleccion != 1 && seleccion != 2 &&
+            seleccion != 3 && seleccion != 4);
+    }
+
+    // do something with selection here
+    // such as a switch statement
+
+    std::cout << "Has seleccionado la opción #" << seleccion << "\n";
+
+    return 0;
+}*/
+
+
+
+
+/*#include <iostream>
+
+int main()
+{
     setlocale(LC_ALL, "es_ES.UTF-8");
     // seleccion debe estar declarado fuera del loop while para poder usarlo más tarde
     int seleccion{ 0 };
@@ -23,4 +99,4 @@ int main()
     std::cout << "Has seleccionado la opción #" << seleccion << "\n";
 
     return 0;
-}
+}*/
